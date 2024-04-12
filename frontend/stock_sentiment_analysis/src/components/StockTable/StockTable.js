282a705c05cc0db48bdac04650d172378e5adba1
Weijie Chen <82107874+wxc2710@users.noreply.github.com>
2024-04-12 19:17:48 -0400

    changes

--- frontend/stock_sentiment_analysis/src/components/StockTable/StockTable.js
+++ frontend/stock_sentiment_analysis/src/components/StockTable/StockTable.js
@@ -1,30 +1,49 @@
 import React, { useState, useEffect } from 'react';
+import './stockTable.css'; 
 
 const StockTable = () => {
   const [companyData, setCompanyData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('/stock_data.json');
         const data = await response.json();
-        console.log('Company data:', data);
         setCompanyData(data);
       } catch (error) {
         console.error('Error fetching company data:', error);
+        setError('Failed to fetch company data');
       }
+      setLoading(false);
     };
 
     fetchData();
   }, []);
 
+
+  const formatMarketCap = (value) => {
+    if (value >= 1e12) {
+      return `${(value / 1e12).toFixed(1)} trillion`;
+    } else if (value >= 1e9) {
+      return `${(value / 1e9).toFixed(1)} billion`;
+    } else if (value >= 1e6) {
+      return `${(value / 1e6).toFixed(1)} million`;
+    } else {
+      return value.toString();
+    }
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
-    <div>
+    <div className="stock-table-container">
       <h1>Company Information</h1>
-      {companyData ? (
-        <table>
-          <tbody>
-            <tr>
+      <table>
+        <tbody>
+          <tr>
               <td><strong>Long Name:</strong></td>
               <td>{companyData.longName}</td>
               <td><strong>Open:</strong></td>
@@ -56,7 +75,7 @@ const StockTable = () => {
             </tr>
             <tr>
               <td><strong>Market Cap:</strong></td>
-              <td>${companyData.marketCap}</td>
+              <td>${formatMarketCap(companyData.marketCap)}</td>
               <td><strong>Beta (5Y Monthly):</strong></td>
               <td>{companyData.beta}</td>
             </tr>
@@ -80,9 +99,6 @@ const StockTable = () => {
             </tr>
           </tbody>
         </table>
-      ) : (
-        <p>Loading...</p>
-      )}
     </div>
   );
 };
