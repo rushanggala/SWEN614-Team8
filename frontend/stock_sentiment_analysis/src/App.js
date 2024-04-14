c0a8599285e712509e360e4c946148ad45eb986b
gupta-shlok <shlokgupta96@gmail.com>
2024-04-13 20:12:24 -0400

    API Call Correction

--- frontend/stock_sentiment_analysis/src/App.js
+++ frontend/stock_sentiment_analysis/src/App.js
@@ -1,35 +1,14 @@
-import React, { useEffect, useState }  from 'react';
 import HomePage from './components/HomePage/HomePage';
 import {StockDataProvider} from "./context/StockDataContext";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CustomSentiment from './components/CustomSentiment/CustomSentiment';
 import './App.css';
-import { getStockData } from './apis/api';
 import Layout from './components/Layout/Layout';
 import CompanyPage from './components/CompanyPage/CompanyPage';
 
 const App = () => {
-    const [stockData, setStockData] = useState(null);
-
-    useEffect(() => {
-        getStockData().then(data => {
-            setStockData(data);
-        }).catch(error => {
-            console.error("Failed to fetch stock data:", error);
-        });
-    });
-
-    if (!stockData) {
-        return (
-            <div className="loader-container">
-                <div className="loader"></div>
-                <div className="loader-text">Loading the website...</div>
-            </div>
-        );
-    }
-
     return (
-        <StockDataProvider value={stockData}>
+        <StockDataProvider>
             <Router>
                 <Layout>
                     <Routes>
--- frontend/stock_sentiment_analysis/src/context/StockDataContext.js
+++ frontend/stock_sentiment_analysis/src/context/StockDataContext.js
@@ -1,19 +1,35 @@
 // StockDataContext.js
-import React, { createContext, useState, useEffect } from 'react';
-import { getStockPrice } from '../apis/api';
+import React, {createContext, useState, useEffect} from 'react';
+import {getStockPrice} from '../apis/api';
 
 export const StockDataContext = createContext([]);
 
-export const StockDataProvider = ({ children }) => {
+export const StockDataProvider = ({children}) => {
     const [stockData, setStockData] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         getStockPrice()
-            .then(data => setStockData(data))
-            .catch(error => console.error(error));
+            .then(data => {
+                setStockData(data);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error(error);
+                setIsLoading(false);
+            });
     }, []);
 
-    const stockArray = Object.entries(stockData).map(([name, data]) => ({ name, ...data }));
+    if (isLoading) {
+        return (
+            <div className="loader-container">
+                <div className="loader"></div>
+                <div className="loader-text">Loading the website...</div>
+            </div>
+        );
+    }
+
+    const stockArray = Object.entries(stockData).map(([name, data]) => ({name, ...data}));
     console.log(stockArray);
     return (
         <StockDataContext.Provider value={stockArray}>
